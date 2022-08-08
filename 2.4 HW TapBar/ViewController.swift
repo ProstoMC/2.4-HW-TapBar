0e0e3c47a84f775793181fdeff86637814c9f1a6
ProstoMC <l-63@inbox.ru>
2022-08-08 10:55:32 +0300

    Initial Commit

---
+++ 2.4 HW TapBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.4 HW TapBar
+//
+//  Created by Admin on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
